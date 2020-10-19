72f66f23ed1c36fc324a2cd4c70cce2aeddc2804
Avram <avramdjordjevic2@gmail.com>
2020-10-20 00:08:50 +0200

    working oop meme

--- test.py
+++ test.py
@@ -16,40 +16,6 @@ groups = [(x.get_text(), x['value']) for x in base_soup.find_all('option', value
 # izbaci seminarski
 groups.pop()
 
-for group in groups.keys():
-
-    print("Working on: " + group, file=sys.stderr)
-
-    resp = req.get(base_link + group)
-    content = resp.content.decode('utf-8')
-    bs_obj = BeautifulSoup(content, 'html.parser')
-
-    # Odmah u startu uklanjamo sve <br> i pretvaramo u '\n' jer kasnije olaksava parsiranje
-    for br in bs_obj.find_all('br'):
-        br.replace_with('\n')
-
-    # Redovi (dani) glavne radne tabele
-    rows = []
-    rows.append(bs_obj.find('td', {"bgcolor": re.compile(".*")}).find_parent())
-    rows.extend(rows[0].find_next_siblings('tr'))
-
-    # 5 Dana u radnoj nedelji
-    for row_index in range(len(rows)):
-
-        time = 8
-
-        # Pocinjemo od indeksa 1 jer preskacemo td sa radnim danom
-        for td in rows[row_index].find_all('td')[1::]:
-
-            duration = 1
-            if td.has_attr('colspan'):
-                duration = int(td['colspan'])
-
-                courses.append(get_course(
-                    td, row_index, teachers[teacher], time))
-
-            time += duration
-
 modules = {}
 
 #put groups under corresponding modules
