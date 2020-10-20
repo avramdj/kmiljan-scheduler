20123024863a4083a231f470b518fe62b1f468c9
Avram <avramdjordjevic2@gmail.com>
2020-10-20 14:28:31 +0200

    pickling update logic

--- data_server.py
+++ data_server.py
@@ -1,4 +1,6 @@
 import sys
+import os
+from glob import glob
 import pickle
 from pprint import pprint
 from kmiljan_parser import download_courses, download_modules
@@ -15,7 +17,10 @@ def fetch_courses():
         all_modules = pickle.load(open(f'pickles/modules_{today}.p', 'rb'))
     except FileNotFoundError:
 
-        
+        old_pickles = glob('./pickles/*.p')
+
+        for file in old_pickles:
+            os.remove(file)
 
         all_courses = download_courses()
         all_modules = download_modules(all_courses)
