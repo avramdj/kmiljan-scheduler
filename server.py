b1769d41b68a89fff0e1cacee43e67f1088303d8
Avram <avramdjordjevic2@gmail.com>
2020-10-20 15:36:26 +0200

    flasked

--- server.py
+++ server.py
@@ -3,16 +3,28 @@ import json
 import sys
 import pickle
 from pprint import pprint
+from werkzeug.routing import BaseConverter
 from scheduler import Scheduler
 from data_server import DataServer
 
 app = Flask(__name__)
 data = DataServer()
 
-@app.route('/')
+class RegexConverter(BaseConverter):
+    def __init__(self, url_map, *items):
+        super(RegexConverter, self).__init__(url_map)
+        self.regex = items[0]
+
+app.url_map.converters['regex'] = RegexConverter
+
+@app.route('/api')
 def hello():
     return data.get_modules()
 
-@app.route('/i')#, methods=['GET', 'POST'])
-def hello_world():
-    return data.get_modules()['i']
+@app.route('/api/<regex("[imnvrl]"):uid>/')#, methods=['GET', 'POST'])
+def smer(uid):
+    return data.get_modules()[uid]
+
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=3333)
