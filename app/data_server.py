2ce8455f5f8b6354703433877d81c73a0191f4e7
avramdj <avramdjordjevic2@gmail.com>
2022-05-28 20:14:47 +0200

    dockerization refactor

--- app/data_server.py
+++ app/data_server.py
@@ -1,15 +1,9 @@
-import sys
-import os
-from glob import glob
-import pickle
-from pprint import pprint
+from datetime import date
+
 from kmiljan_parser import download_courses, download_modules
-from scheduler import Scheduler
-from datetime import date, datetime
 
 
 class DataServer:
-    
     def __init__(self):
         self.all_courses = []
         self.all_modules = {}
@@ -17,31 +11,35 @@ class DataServer:
         self.update()
 
     def get_courses(self):
-        #fetch and periodically update course information
+        # fetch and periodically update course information
         today = date.today().strftime("%b_%d_%Y")
-        
+
         if self.last_update != today:
             self.update()
         return self.all_courses
 
     def get_modules(self):
-        #fetch and periodically update module information
+        # fetch and periodically update module information
         today = date.today().strftime("%b_%d_%Y")
-        
+
         if self.last_update != today:
             self.update()
         return self.all_modules
 
     def update(self):
-        self.all_courses = download_courses() #pickle.load(open('pickles/courses.p', 'rb')) 
-        self.all_modules = download_modules(self.all_courses) #pickle.load(open('pickles/modules.p', 'rb'))
+        self.all_courses = (
+            download_courses()
+        )  # pickle.load(open('pickles/courses.p', 'rb'))
+        self.all_modules = download_modules(
+            self.all_courses
+        )  # pickle.load(open('pickles/modules.p', 'rb'))
 
-        #MINI IZMENE
+        # MINI IZMENE
         if len(self.all_courses) == 0:
             return
 
-        #pickle.dump(self.all_courses, open('pickles/courses.p', 'wb'))
-        #pickle.dump(self.all_modules, open('pickles/modules.p', 'wb'))
+        # pickle.dump(self.all_courses, open('pickles/courses.p', 'wb'))
+        # pickle.dump(self.all_modules, open('pickles/modules.p', 'wb'))
 
         today = date.today().strftime("%b_%d_%Y")
         self.last_update = today
--- config.py
+++
@@ -1,1 +0,0 @@
-max_response_size = 20
