eb76f165b78b23397fb38f9a216c37c2241b6935
Nikola Sutic <nikolasutic@protonmail.com>
2022-09-30 20:27:08 +0200

    Proxy logger IP fix.

--- app/logger.py
+++ app/logger.py
@@ -2,4 +2,6 @@ import datetime as dt
 
 def log_request(request, filename = "logfile.txt"):
     with open(filename, "a") as f:
-        f.write('[%s] {%s} {%s}\n' % (dt.datetime.now(), request.remote_addr, request.full_path))
+        remote_ip = request.access_route[0] if request.access_route != None and len(request.access_route) != 0 \
+            else request.remote_addr
+        f.write('[%s] {%s} {%s}\n' % (dt.datetime.now(), remote_ip, request.full_path))
